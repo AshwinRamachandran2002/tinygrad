612dc8fe2efeba28d92079923b3f8c3d4e2437ae
AshwinRamachandran2002 <200050016@iitb.ac.in>
2024-03-24 20:18:15 +0530

    fix for llvm

--- tinygrad/renderer/llvmir.py
+++ tinygrad/renderer/llvmir.py
@@ -72,15 +72,11 @@ def uops_to_llvm_ir(function_name:str, uops:UOpGraph) -> str:
   module = ir.Module(name=__file__)
 
   # extract global buffers (NOTE: this isn't right if DEFINE_GLOBAL is out of order)
-  buf_to_dtype = {}
-  for i,u in enumerate(uops):
-    if u.uop is UOps.DEFINE_GLOBAL:
-      while i != u.arg[0]: 
-        buf_to_dtype[(i, f"fake_{i}", False)] = PtrDType(dtypes.float)
-        i += 1
-      buf_to_dtype[u.arg] = u.dtype
-    elif u.uop is UOps.DEFINE_VAR:
-      buf_to_dtype[u.arg] = u.dtype
+  buf_to_dtype = {u.arg: u.dtype for u in uops if u.uop is UOps.DEFINE_VAR}
+  for i, u in enumerate(uops): 
+    if u.uop is UOps.DEFINE_GLOBAL: 
+      buf_to_dtype.update({(j, f"fake_{j}", False):PtrDType(dtypes.float) for j in range(i, u.arg[0])})
+      buf_to_dtype.update({u.arg: u.dtype})
   buf_index = {x:i for i,x in enumerate(buf_to_dtype.keys())}
 
   # create llvm function
